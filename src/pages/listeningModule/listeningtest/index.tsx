218fcb27a81ad840dc8ec54616bf456354e3ed76
Alex <majestyye@gmail.com>
2023-12-07 16:48:40 +0800

    feat: 填空题

--- src/pages/listeningModule/listeningtest/index.tsx
+++ src/pages/listeningModule/listeningtest/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./styles.module.less";
 import rangy from "rangy";
 import "rangy/lib/rangy-classapplier";
@@ -19,23 +19,10 @@ const questionModule: any[] = [
       {
         type: "fill_in_blanks",
         title: "Complete the notes below. Write ONE WORD ONLY for each answer.",
-        article_content: (
-          <div>
-            The total rent: Peter £ 110 & Jim £
-            <Input className={styles.input_style} placeholder="1" />
-            <br />
-            Car parking: in the
-            <Input className={styles.input_style} placeholder="2" />
-            <br />
-            A place to buy things:
-            <Input className={styles.input_style} placeholder="3" />, because
-            Jim works there.
-            <br />
-            The fees they should share:
-            <Input className={styles.input_style} placeholder="4" />
-            fees
-          </div>
-        ),
+        article_content: `The total rent: Peter £ 110 & Jim £ &#8203;【blank】&#8203;<br/>
+          Car parking: in the &#8203;【blank】&#8203;<br/>
+          A place to buy things: &#8203;【blank】&#8203;, because Jim works there.<br/>
+          The fees they should share: &#8203;【blank】&#8203; fees<br/>`,
         question_list: [
           {
             id: 1,
@@ -182,6 +169,13 @@ const ListeningTest: React.FC = () => {
   const [highlighter, setHighlighter] = useState<any>(null);
   const [selectionRange, setSelectionRange] = useState<any>(null);
   const [selection, setSelection] = useState<any>(null);
+  const [currentFocus, setCurrentFocus] = useState({ type: "choice", idx: 0 });
+  // const fillInBlanksQuestions = questionModule.flatMap((part) =>
+  //   part.type_list
+  //     .filter((type: any) => type.type === "fill_in_blanks")
+  //     .map((type: any) => type.question_list)
+  // );
+  const inputRefs = Array.from({ length: 10 }, () => useRef(null));
 
   useEffect(() => {
     rangy.init();
@@ -198,6 +192,23 @@ const ListeningTest: React.FC = () => {
   const chooseQuestion = (part: number, question: number) => {
     console.log(part, question);
     setPart(part);
+    const targetPart = questionType[part];
+    let typeIndex = 0;
+    let question_count = targetPart.type_list[0].question_list.length;
+    if (targetPart && targetPart.type_list) {
+      while (
+        typeIndex < targetPart.type_list.length &&
+        question_count - 1 < question
+      ) {
+        typeIndex++;
+        question_count +=
+          targetPart.type_list?.[typeIndex]?.question_list.length;
+      }
+    }
+    if (targetPart.type_list[typeIndex].type === "fill_in_blanks") {
+      console.log(inputRefs[typeIndex], "inputRefs");
+      inputRefs[question].current.focus();
+    }
   };
 
   const changeChoice = (e: any, index: number, idx: number, type: string) => {
@@ -210,28 +221,45 @@ const ListeningTest: React.FC = () => {
         break;
       case "multi_choice":
         questionType[part].type_list[index].question_list[idx].answer = e;
+        break;
+      case "fill_in_blanks":
+        questionType[part].type_list[index].question_list[idx].answer =
+          e.target.value;
+        break;
     }
     setQuestionType([...questionType]);
   };
 
   const handleSelect = (e: any) => {
-    console.log("onMouseUp");
-    setShowMark(false);
-    const sel = rangy.getSelection();
-    const _selectionRange = sel.getRangeAt(0);
-    const text = _selectionRange.toString();
+    try {
+      console.log("onMouseUp");
+      setShowMark(false);
+      const sel = rangy.getSelection();
+      const _selectionRange = sel.getRangeAt(0);
+      const text = _selectionRange.toString();
 
-    if (text) {
-      setSelection(sel);
-      setSelectionRange(_selectionRange);
-      setShowMark(true);
-      setMousePosition([e.clientX, e.clientY]);
+      if (text) {
+        setSelection(sel);
+        setSelectionRange(_selectionRange);
+        setShowMark(true);
+        setMousePosition([e.clientX, e.clientY]);
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
   const handleMark = () => {
     setShowMark(false);
   };
+
+  const focusQues = (type: string, idx: number) => {
+    setCurrentFocus({
+      type,
+      idx,
+    });
+  };
+
   return (
     <div className={styles.step_content} onMouseUp={handleSelect}>
       <TestHeader />
@@ -254,50 +282,79 @@ const ListeningTest: React.FC = () => {
         <Card className={`${styles.test_card} overflow_auto`}>
           {questionType[part].type_list.map((item: any, index: number) => (
             <div key={index}>
-              <p>{item.title}</p>
-              {item.type === "fill_in_blanks" && item.article_content}
-              {item.type === "choice" &&
-                item.question_list.map((i: any, idx: number) => (
-                  <div key={idx}>
-                    <p>{`${i.no} ${i.question}`}</p>
-                    <Radio.Group
-                      value={i.answer}
-                      onChange={(e) => changeChoice(e, index, idx, item.type)}
-                    >
-                      <Space direction="vertical">
-                        {i.options.map((value: any, id: number) => (
-                          <Radio value={value.label} key={id}>
-                            {value.label}. {value.text}
-                          </Radio>
-                        ))}
-                      </Space>
-                    </Radio.Group>
-                  </div>
-                ))}
-              {item.type === "multi_choice" &&
-                item.question_list.map((i: any, idx: number) => (
-                  <div key={idx}>
-                    <p>{`${i.no} ${i.question}`}</p>
-                    <Checkbox.Group
-                      value={i.answer}
-                      onChange={(e) => changeChoice(e, index, idx, item.type)}
-                    >
-                      <Space direction="vertical">
-                        {i.options.map((value: any, id: number) => (
-                          <Checkbox
-                            value={value.label}
-                            key={id}
-                            onChange={(e) => {
-                              e.stopPropagation();
-                            }}
-                          >
-                            {value.label}. {value.text}
-                          </Checkbox>
-                        ))}
-                      </Space>
-                    </Checkbox.Group>
-                  </div>
-                ))}
+              <p className="font-1rem lh-2rem mb-20">{item.title}</p>
+              {item.type === "fill_in_blanks" && (
+                <div className="lh-3rem mb-30">
+                  {item.article_content
+                    .split("【blank】")
+                    .map((i: any, idx: number) => (
+                      <span key={idx}>
+                        <span dangerouslySetInnerHTML={{ __html: i }} />
+                        {idx < item.question_list.length && (
+                          <Input
+                            ref={inputRefs[idx]}
+                            type="text"
+                            className={styles.input_style}
+                            value={item.question_list[idx].answer}
+                            placeholder={item.question_list[idx].no}
+                            onChange={(e) =>
+                              changeChoice(e, index, idx, item.type)
+                            }
+                          />
+                        )}
+                      </span>
+                    ))}
+                </div>
+              )}
+              <div className="flex-jcb flex-wrap">
+                {item.type === "choice" &&
+                  item.question_list.map((i: any, idx: number) => (
+                    <div key={idx} style={{ width: "50%" }} className="mb-30">
+                      <p
+                        className={`mb-20 lh-2rem font-1rem fwb`}
+                        onClick={() => focusQues(item.type, idx)}
+                      >{`${i.no} ${i.question}`}</p>
+                      <Radio.Group
+                        style={{ fontSize: "1rem" }}
+                        value={i.answer}
+                        onChange={(e) => changeChoice(e, index, idx, item.type)}
+                      >
+                        <Space direction="vertical">
+                          {i.options.map((value: any, id: number) => (
+                            <Radio value={value.label} key={id}>
+                              {value.label}. {value.text}
+                            </Radio>
+                          ))}
+                        </Space>
+                      </Radio.Group>
+                    </div>
+                  ))}
+                {item.type === "multi_choice" &&
+                  item.question_list.map((i: any, idx: number) => (
+                    <div key={idx} style={{ width: "50%" }} className="mb-30">
+                      <p className="mb-20 lh-2rem font-1rem fwb">{`${i.no} ${i.question}`}</p>
+                      <Checkbox.Group
+                        style={{ fontSize: "1rem" }}
+                        value={i.answer}
+                        onChange={(e) => changeChoice(e, index, idx, item.type)}
+                      >
+                        <Space direction="vertical">
+                          {i.options.map((value: any, id: number) => (
+                            <Checkbox
+                              value={value.label}
+                              key={id}
+                              onChange={(e) => {
+                                e.stopPropagation();
+                              }}
+                            >
+                              {value.label}. {value.text}
+                            </Checkbox>
+                          ))}
+                        </Space>
+                      </Checkbox.Group>
+                    </div>
+                  ))}
+              </div>
             </div>
           ))}
         </Card>
